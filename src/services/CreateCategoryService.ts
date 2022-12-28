cdb440788ea433aaad5c9168d069718fd777a0a9
viniciusventura29 <viniciusventura29@gmail.com>
2022-12-28 19:32:19 -0300

    📦 NEW: Category service file

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,23 @@
+import { ICategoryRepository } from "../repositories/ICategoriesRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+class CreateCategoryService {
+    constructor(private categoriesRespository: ICategoryRepository) {}
+
+    execute({ name, description }: IRequest): void {
+        const categoryAlreadyExists =
+            this.categoriesRespository.findByName(name);
+
+        if (categoryAlreadyExists) {
+            throw new Error("Category already exists!");
+        }
+
+        this.categoriesRespository.create({ name, description });
+    }
+}
+
+export { CreateCategoryService };
