3df6c202630c096a3d4b6985cfa6557031f7db3b
viniciusventura29 <viniciusventura29@gmail.com>
2023-01-05 02:40:52 -0300

    📦 NEW: Create specification router

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,21 +1,10 @@
 import { Router } from "express";
-import { SpecificationRepository } from "../modules/cars/repositories/SpecifitcationRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/UseCases/CreateSpecification";
 
 const specificationRoutes = Router();
 
-const specificationRepository = new SpecificationRepository();
-
 specificationRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
-
-    const createSpecificationService = new CreateSpecificationService(
-        specificationRepository
-    );
-
-    createSpecificationService.execute({ name, description });
-
-    return response.status(201).send();
+    createSpecificationController.handle(request, response);
 });
 
 export { specificationRoutes };
