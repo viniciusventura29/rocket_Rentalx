6d0926d72648854e74c6283f9c7450d0ac421a36
viniciusventura29 <viniciusventura29@gmail.com>
2023-01-06 00:25:40 -0300

    📦 NEW: Instance imports categories files

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/UseCases/CreateCategory";
+import { importCategoryController } from "../modules/cars/UseCases/ImportCategory";
 import { listCategoryController } from "../modules/cars/UseCases/ListCategory";
 
 const categoriesRoutes = Router();
@@ -9,17 +10,15 @@ const categoriesRoutes = Router();
 const upload = multer({ dest: "./tmp" });
 
 categoriesRoutes.post("/", (request, response) => {
-    createCategoryController.handle(request, response);
+    return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-    listCategoryController.handle(request, response);
+    return listCategoryController.handle(request, response);
 });
 
 categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
-    const { file } = request;
-    console.log(file);
-    return response.status(200).send();
+    return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
