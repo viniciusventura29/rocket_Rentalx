cb4a1e423fbd02385b1a4a46a5bb25f6ce899fa0
viniciusventura29 <viniciusventura29@gmail.com>
2023-01-07 16:59:38 -0300

    📦 NEW: Connecting import categories to repository

--- src/modules/cars/UseCases/ImportCategory/index.ts
+++ src/modules/cars/UseCases/ImportCategory/index.ts
@@ -1,7 +1,10 @@
+import { CategoryRepository } from "../../repositories/implementations/CategoriesRespository";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-const importCategoryUseCase = new ImportCategoryUseCase();
+const categoryRepository = CategoryRepository.getInstance();
+
+const importCategoryUseCase = new ImportCategoryUseCase(categoryRepository);
 
 const importCategoryController = new ImportCategoryController(
     importCategoryUseCase
