d9c86891f89fa52c49774a636e104caf027f6605
viniciusventura29 <viniciusventura29@gmail.com>
2023-01-07 16:59:58 -0300

    📦 NEW: Reading datas from upload

--- src/modules/cars/UseCases/ImportCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/UseCases/ImportCategory/ImportCategoryUseCase.ts
@@ -1,17 +1,47 @@
 import { parse as csvParse } from "csv-parse";
 import fs from "fs";
 
+import { CategoryRepository } from "../../repositories/implementations/CategoriesRespository";
+
+interface IImportCategory {
+    name;
+    description;
+}
+
+const categories = [];
+
 class ImportCategoryUseCase {
-    execute(file: Express.Multer.File): void {
-        const stream = fs.createReadStream(file.path);
+    constructor(private categoryRepository: CategoryRepository) {}
 
-        const parseFile = csvParse();
+    loadCatergories(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path);
 
-        stream.pipe(parseFile);
+            const parseFile = csvParse();
 
-        parseFile.on("data", async (line) => {
-            console.log(line);
+            stream.pipe(parseFile);
+
+            parseFile
+                .on("data", async (line) => {
+                    const [name, description] = line;
+
+                    categories.push({
+                        name,
+                        description,
+                    });
+                })
+                .on("end", () => {
+                    resolve(categories);
+                })
+                .on("error", (err) => {
+                    reject(err);
+                });
         });
+    }
+
+    async execute(file: Express.Multer.File): Promise<void> {
+        const categories = await this.loadCatergories(file);
+        console.log(categories);
     }
 }
 
